7a6e416d4a47fd19ed8f3e3395c43c7d742758c2
Avalanche322 <avalfnche@gmail.com>
2021-03-11 15:17:43 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# BEST-CREATIVE-STUDIO
+
+This project I am doing to study Bootstrap with SCSS. This site is suitable for all devices with different resolutions and animations. In my work I used VS Code, Marcy (this is a website to use the layout) and a layout that I took from https://camouf.ru/designes/ready-psd-layouts-for-saito/
---
+++ js/script.js
@@ -0,0 +1,93 @@
+/*Preloader*/
+function loadData() {
+	return new Promise((resolve, reject) => {
+		setTimeout(resolve, 2000);
+	})
+}
+loadData().then(() => {
+	let preloaderEl = document.getElementById('loader');
+	preloaderEl.classList.add('hidden');
+	$('body').removeClass('loading');
+	preloaderEl.classList.remove('visible');
+});
+
+/*Active portfolio nav*/
+$(function () {
+	$(".portfolio__work-button").on('click', function () {
+		$('.portfolio__work-button').removeClass('active')
+
+		if ($(this).attr('aria-expanded') !== false) {
+			$(this).addClass('active')
+		}
+	})
+});
+
+/*Slider toggle*/
+$(".fa-times").click(function () {
+	$(".carousel-inner").toggle("d-none");
+	if ($(this).hasClass('fa-times')) {
+		$(this).removeClass('fa-times');
+		$(this).addClass('fa-check');
+	} else {
+		$('fas fa-times').removeClass('fa-times');
+		$(this).addClass('fa-times');
+	}
+});
+
+/*Header click menu burger - body lock*/
+$(document).ready(function () {
+	$('.navbar-toggler').click(function (event) {
+		$('.header__burger').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});
+
+/*Add style for contact input*/
+$('.form-control').focus(function () {
+	$(this).parent().find('.input-group-text').css({'border-color': '#f39c12', 'color': '#f39c12'});
+	$(this).css('border-color', '#f39c12');
+	$(this).parent().find('.far').css('color', '#f39c12');
+});
+$('.form-control').focusout(function () {
+	$(this).parent().find('.input-group-text').css({ 'border-color': '#ced4da', 'color': '#808080'});
+	$(this).css('border-color', '#ced4da');
+	$(this).parent().find('.far').css('color', '#808080');
+});
+
+/*Common Animation*/
+const animItems = document.querySelectorAll('._anim-items');
+
+if (animItems.length > 0) {
+	window.addEventListener('scroll', animOnScroll);
+	function animOnScroll() {
+		for (let index = 0; index < animItems.length; index++) {
+			const animItem = animItems[index];
+			const animItemHeight = animItem.offsetHeight;
+			const animItemOffset = offset(animItem).top;
+			const animStart = 4;
+
+			let animItemPoint = window.innerHeight - animItemHeight / animStart;
+			if (animItemHeight > window.innerHeight) {
+				animItemPoint = window.innerHeight - window.innerHeight / animStart;
+			}
+
+			if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+				animItem.classList.add('_active');
+			} else {
+				if (!animItem.classList.contains('_anim-no-hide')) {
+					animItem.classList.remove('_active');
+				}
+			}
+		}
+	}
+	function offset(el) {
+		const rect = el.getBoundingClientRect(),
+			scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+			scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+		return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+	}
+	setTimeout(() => {
+		animOnScroll();
+	}, 1500);
+}
+
